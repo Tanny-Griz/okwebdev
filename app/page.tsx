68ba16a35be28e54cace81be44d70591cd319d0a
Tanya <tanya.griz89@gmail.com>
2026-06-05 16:05:12 -0500

    add Why section

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,10 @@ import { Hero } from "@/components/hero";
 import { Projects } from "@/components/projects";
 import { Stack } from "@/components/stack";
 import { Contact } from "@/components/contact";
-import { Experience } from "@/components/experience";
 import { Footer } from "@/components/footer";
 import { Services } from "@/components/services";
 import { HowItWorks } from "@/components/HowItWorks";
+import { WhyWorkWithMe } from "@/components/WhyWorkWithMe";
 
 export default function Home() {
   return (
@@ -18,7 +18,7 @@ export default function Home() {
         <HowItWorks />        
         <Projects />
         <Stack />
-        <Experience />
+        <WhyWorkWithMe />
         <Contact />
         <Footer />
       </main>
